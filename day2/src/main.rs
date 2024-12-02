8bdc2aa2d235f6c527ce993a1355ffc25a9829ac
Koulie <kouliekyriakos@hotmail.com>
2024-12-02 16:10:48 -0500

    Finsihed day 2 star one

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,9 +5,12 @@ const FILE_NAME: &str = "data.txt";
 
 fn main() {
     println!("Hello, world!");
-
     let string_vector: Vec<String> = file_to_string_vec(FILE_NAME);
+    println!("Hello, world!2");
     println!("This is the first line of the string vector {}", string_vector[0]);
+    let final_count: usize = check_all_lines(&string_vector);
+    println!("The final count is {}", final_count);
+
 }
 fn file_to_string_vec(file_name: &str) -> Vec<String> {
     let mut string_vector: Vec<String> = Vec::new();
@@ -28,13 +31,64 @@ fn string_vec_to_int_vec(input_string: &String) -> Vec<usize> {
     }
     vec_usize
 }
-fn check_safety_of_vec(input_vec: Vec<usize>) -> bool {
+
+fn check_safety_of_vec(input_vec: &Vec<usize>) -> bool {
     let length: usize = input_vec.len() - 1;
-    let i: usize = 0;
-    let flag = true;
+    let mut i: usize = 0;
+    let mut flag = true;
     while i < length {
-        let abs_diff: usize = (input_vec[i].into(isize) - input_vec[i + 1].into(isize)).abs
-        if input_vec[i]
+        let abs_diff: usize = (input_vec[i] as isize - input_vec[i + 1] as isize).abs() as usize;
+        if abs_diff < 1 || abs_diff > 3 {
+            flag = false;
+        }
+        i += 1;
     }
-    flag
+
+    flag && is_increasing_or_decreasing(&input_vec)
+}
+
+fn is_increasing_or_decreasing(input_vec: &Vec<usize>) -> bool {
+    let is_increasing: bool;
+    let mut is_consistent: bool = true;
+    let length: usize = input_vec.len() - 1;
+    let mut i: usize = 0;
+    if input_vec[1] > input_vec[0] {
+        is_increasing = true;
+    } else {
+        is_increasing = false;
+    }
+    while i < length {
+        if is_increasing && input_vec[i] >= input_vec[i + 1]{
+            is_consistent = false;
+        }
+        if !is_increasing && input_vec[i] <= input_vec[i + 1]{
+            is_consistent = false;
+        }
+        i += 1;
+    }
+    is_consistent
+
+}
+fn convert_all_lines_to_ints(string_vector: &Vec<String>) -> Vec<Vec<usize>> {
+    let mut return_vector: Vec<Vec<usize>> = Vec::new();
+    for line in string_vector {
+        return_vector.push(string_vec_to_int_vec(&line))
+    }
+    return_vector
+}
+
+fn check_all_lines(input_lines: &Vec<String>) -> usize {
+    let mut count: usize = 0;
+    let int_vector: Vec<Vec<usize>> = convert_all_lines_to_ints(&input_lines);
+    for line in int_vector {
+        println!("Checking a line!");
+        if check_safety_of_vec(&line) == true {
+            count += 1;
+            println!("Found a safe line, current count = {}", count);
+            for i in &line {
+                print!(" {}", i);
+            }
+        }
+    }
+    count
 }
