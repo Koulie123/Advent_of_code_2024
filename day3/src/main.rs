ebd8431017e08552ed3e56d81f8cc65f5a73426b
Koulie <kouliekyriakos@hotmail.com>
2024-12-03 16:29:10 -0500

    Started making basic functions

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -12,3 +12,24 @@ fn read_to_string(file_name: &str) -> String {
     let message: String = fs::read_to_string(file_name).expect("Did not find the file");
     message
 }
+
+fn return_locations_of_muls (string_array: &String) -> Vec<usize> {
+    let char_vector: Vec<char> = string_array.chars().collect();
+    let mut vector_to_return: Vec<usize> = Vec::new();
+    // let mut i: usize = 0;
+    for i in 0..(char_vector.len()-2) {
+        if check_for_mul(&char_vector[i..i+3]){
+            vector_to_return.push(i);
+        }
+
+    }
+    vector_to_return
+}
+
+fn check_for_mul(slice: &[char]) -> bool {
+    let mut flag: bool = false;
+    if slice[0] == 'm' && slice[1] == 'u' && slice[2] == 'l'{
+        flag = true;
+    }
+    flag
+}
