7290632eae0baef52071ecd9d093dfd33a5f63d8
Koulie <kouliekyriakos@hotmail.com>
2024-12-04 10:06:43 -0500

    added function to search all horizonal xmases

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,22 @@
 use std::fs::read_to_string;
 
 fn main() {
-    println!("Hello, world!");
+    let mut total_count: usize = 0;
+    let mut char_vector: Vec<Vec<char>> = Vec::new();
+    let file_name: String = String::from("data.txt");
+    let vector_string = file_to_readlines(file_name);
+    for line in vector_string {
+        let temp_line = convert_string_to_char_array(line);
+        char_vector.push(temp_line);
+    }
+    for i in 0..10 {
+        println!("{}", char_vector[i][0]);
+    }
+    for i in 0..4 {
+        total_count += search_horizontal(&char_vector[i])
+    }
+    println!("The total number of xmases in the first 4 lines is {}", total_count);
+
 }
 
 fn file_to_readlines(file_name: String) -> Vec<String> {
@@ -13,8 +28,21 @@ fn file_to_readlines(file_name: String) -> Vec<String> {
     result
 }
 
-fn convert_string_to_char_array(input_string: &String) -> Vec<char> {
+fn convert_string_to_char_array(input_string: String) -> Vec<char> {
     let str_copy = input_string.clone();
     let char_array = str_copy.chars().collect();
     char_array
+}
+
+fn search_horizontal(chars: &Vec<char>) -> usize {
+    let mut count: usize = 0;
+    let length: usize = chars.len();
+    let forward_xmas: Vec<char> = vec!['X', 'M', 'A', 'S'];
+    let backward_xmas: Vec<char> = vec!['S', 'A', 'M', 'X'];
+    for i in 0..(length - 4) {
+        if chars[i..i+4] == forward_xmas || chars[i..i+4] == backward_xmas{
+            count += 1;
+        }
+    }
+    count   
 }
