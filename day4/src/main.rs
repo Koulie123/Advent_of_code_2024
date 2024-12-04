7193f827210a2fa80f5fb431338ed3f2844b8bfc
Koulie <kouliekyriakos@hotmail.com>
2024-12-04 15:43:35 -0500

    finished day4 part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::read_to_string;
-
+const FORWARD_XMAS: [char; 4]= ['X', 'M', 'A', 'S'];
+const BACKWARD_XMAS: [char; 4] = ['S', 'A', 'M', 'X'];
 fn main() {
+
     let mut total_count: usize = 0;
     let mut char_vector: Vec<Vec<char>> = Vec::new();
     let file_name: String = String::from("data.txt");
@@ -9,12 +11,12 @@ fn main() {
         let temp_line = convert_string_to_char_array(line);
         char_vector.push(temp_line);
     }
-    for i in 0..10 {
-        println!("{}", char_vector[i][0]);
-    }
-    for i in 0..4 {
+    for i in 0..char_vector.len() {
         total_count += search_horizontal(&char_vector[i])
     }
+    total_count += search_vertical(&char_vector);
+    total_count += search_diagonal_down(&char_vector);
+    total_count += search_diagonal_up(&char_vector);
     println!("The total number of xmases in the first 4 lines is {}", total_count);
 
 }
@@ -37,12 +39,63 @@ fn convert_string_to_char_array(input_string: String) -> Vec<char> {
 fn search_horizontal(chars: &Vec<char>) -> usize {
     let mut count: usize = 0;
     let length: usize = chars.len();
-    let forward_xmas: Vec<char> = vec!['X', 'M', 'A', 'S'];
-    let backward_xmas: Vec<char> = vec!['S', 'A', 'M', 'X'];
-    for i in 0..(length - 4) {
-        if chars[i..i+4] == forward_xmas || chars[i..i+4] == backward_xmas{
+    for i in 0..(length - 3) {
+        if chars[i..i+4] == *FORWARD_XMAS.as_slice() || chars[i..i+4] == *BACKWARD_XMAS.as_slice(){
             count += 1;
         }
     }
     count   
+}
+
+fn search_vertical(chars: &Vec<Vec<char>>) -> usize {
+    let height: usize = chars[0].len();
+    let length: usize = chars.len();
+    let mut count: usize = 0;
+    for j in 0..height {
+        for i in 0..length-3 {
+            let mut slice: Vec<char> = Vec::new();
+            for k in 0..4 {
+                slice.push(chars[i+k][j])
+            }
+            if slice == FORWARD_XMAS || slice == BACKWARD_XMAS {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+fn search_diagonal_down(chars: &Vec<Vec<char>>) -> usize{
+    let height: usize = chars[0].len();
+    let length: usize = chars.len();
+    let mut count: usize = 0;
+    for i in 0..length-3 {
+        for j in 0..height-3 {
+            let mut slice: Vec<char> = Vec::new();
+            for k in 0..4 {
+                slice.push(chars[i+k][j+k]);
+            }
+            if slice == FORWARD_XMAS || slice == BACKWARD_XMAS {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+fn search_diagonal_up(chars: &Vec<Vec<char>>) -> usize {
+    let height: usize = chars[0].len();
+    let length: usize = chars.len();
+    let mut count: usize = 0;
+    for i in 0..length-3 {
+        for j in 3..height {
+            let mut slice: Vec<char> = Vec::new();
+            for k in 0..4 {
+                slice.push(chars[i+k][j-k]);
+            }
+            if slice == FORWARD_XMAS || slice == BACKWARD_XMAS {
+                count += 1;
+            }
+        }
+    }
+    count
 }
